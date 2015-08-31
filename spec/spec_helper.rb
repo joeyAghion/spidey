09e836fd0a9914db1b1a798fb5d905321db1e4bf
dblock <dblock@dblock.org>
2015-08-31 08:35:56 -0400

    Upgraded RSpec via Transpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-$:.unshift(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require 'spidey'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
+  config.raise_errors_for_deprecations!
   config.filter_run :focus
 end
